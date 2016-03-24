376167c123fd54437ad7b0e328c05feb025a1f41
agonyeye <agonyeye@hotmail.com>
2016-03-24 12:34:56 -0400

    this is a testing

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Good afternoon Appsoar! I have been seen you %s times.' % redis.get('hits')
+    return 'Good afternoon Appsoar! I have been seen you %d times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -2,13 +2,10 @@
 cd ${WORKSPACE}/src
 docker build -t 192.168.180.100:80/python-redis-demo:${BUILD_NUMBER} .
 docker push 192.168.180.100:80/python-redis-demo:${BUILD_NUMBER}
-pwd
 cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
-echo $BUILD_NUMBER
 
-sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
-pwd
+sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.ymlpwd
 chmod 777 ./rancher-compose
-echo $BUILD_NUMBER
+
 ./rancher-compose --access-key EF156F5D22D46A81D9B0 --secret-key QMGwZjWJqNaV8F7aiETmzWRo6zJnarBBKzSzUJoh -p python-redis-demo-build up -d
