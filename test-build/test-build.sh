2b2df597a0d73f0050545c9e33638cad687145dc
agonyeye <agonyeye@hotmail.com>
2016-03-24 07:25:55 -0400

    20160325-9

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -9,4 +9,4 @@ sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.
 
 pwd
 chmod 777 ./rancher-compose
-. /rancher-compose --url http://192.168.180.100:8080 --access-key F6E680E61B3FBA2FE465 --secret-key XntX4ocn5Ttxrt3nKVyqVdcrNDZC3pd2Jj31GcnA -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --url http://192.168.180.100:8080 --access-key F6E680E61B3FBA2FE465 --secret-key XntX4ocn5Ttxrt3nKVyqVdcrNDZC3pd2Jj31GcnA -p python-redis-demo-build${BUILD_NUMBER} up -d
