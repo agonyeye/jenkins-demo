dceadd16aa9b19b9fc5ca03adbc0e911e594ff83
agonyeye <agonyeye@hotmail.com>
2016-03-24 07:20:22 -0400

    20160325-7

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -7,4 +7,4 @@ cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 
-./rancher-compose --url http://192.168.180.100:8080 --access-key 34A7CE5B9A9810A47582 --secret-key aoJPNdt8EbMaFbGWz7sr9CMZ1xvnnM8eEGUvNnEu -p python-redis-demo-build${BUILD_NUMBER} up -d
+. /rancher-compose --url http://192.168.180.100:8080 --access-key 34A7CE5B9A9810A47582 --secret-key aoJPNdt8EbMaFbGWz7sr9CMZ1xvnnM8eEGUvNnEu -p python-redis-demo-build${BUILD_NUMBER} up -d
