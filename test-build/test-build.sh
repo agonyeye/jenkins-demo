34db03e3af8ad2f3d09fdebebbe8cf819548affa
agonyeye <agonyeye@hotmail.com>
2016-03-24 22:33:01 -0400

    20160328-4

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -5,4 +5,4 @@ docker push 192.168.180.100:80/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 
-./rancher-compose --access-key EF156F5D22D46A81D9B0 --secret-key QMGwZjWJqNaV8F7aiETmzWRo6zJnarBBKzSzUJoh up --pull  -d --confirm-upgrade  --upgrade python-redis-demo-build${BUILD_NUMBER}
+./rancher-compose --access-key EF156F5D22D46A81D9B0 --secret-key QMGwZjWJqNaV8F7aiETmzWRo6zJnarBBKzSzUJoh up --pull  -d --confirm-upgrade  --upgrade pyapp
